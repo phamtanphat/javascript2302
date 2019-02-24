e2b623bbc343c2893c6a72841f4c114eb3793dce
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-02-24 11:21:39 +0700

    finish

--- d.js
+++ d.js
@@ -1,4 +1,4 @@
-const mangso = [1,2,3,4,5,6,7,8,9];
+// const mangso = [1,2,3,4,5,6,7,8,9];
 
 // mangso.push(10); // Them o cuoi
 // mangso.unshift(11); // Them o dau
@@ -16,13 +16,28 @@ const mangso = [1,2,3,4,5,6,7,8,9];
 // console.log(mangso.reduce((item1 , item2) => item1 - item2));
 // console.log(mangso.every(item => item > 0));
 
-const mangPerson = [
-    {name : "Ti" , age : 20},
-    {name : "Tun" , age : 25},
-    {name : "Tin" , age : 30},
-    {name : "Thu" , age : 35},
-]
+// const mangPerson = [
+//     {name : "Ti" , age : 20},
+//     {name : "Tun" , age : 25},
+//     {name : "Tin" , age : 30},
+//     {name : "Thu" , age : 35},
+// ]
 
 // console.log(mangPerson.map(person => person.name));
 // console.log(mangPerson.filter(person => person.name == 'Ti'));
-console.log(mangPerson.map(person => person.age).reduce((person1 , person2 ) =>  person1 + person2));
+// console.log(mangPerson.map(person => person.age).reduce((person1 , person2 ) =>  person1 + person2));
+// console.log(mangso.sort((a , b) => b - a));
+
+Array.prototype.myfilter = function(fn){
+    const mang = [];
+    for(let index = 0 ; index < this.length ; index++){
+        const element = this[index];
+        const dk = fn(element);
+        if(dk) mang.push(element);
+    }
+    return mang;
+}
+
+const mangso = [1,2,3,4,5,6,7,8,9];
+
+console.log(mangso.myfilter(item => item > 4));
