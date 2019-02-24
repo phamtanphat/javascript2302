0186bcd125b8f7e4c9d05cc6d4abdc21023ea553
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-02-24 09:49:39 +0700

    dk function

--- b.js
+++ b.js
@@ -1,32 +1,51 @@
 function insochan(){
     for(let i = 0 ; i<=100 ; i++ ){
-        if(i % 2 == 0){
-            console.log(i)
-        }
+        // if(i % 2 == 0){
+        //     console.log(i)
+        // }
+        const dk = i % 2== 0;
+        if(dk) console.log(i);
     }
 }
 function insole(){
     for(let i = 0 ; i<=100 ; i++ ){
-        if(i % 2 == 1){
-            console.log(i)
-        }
+        // if(i % 2 == 1){
+        //     console.log(i)
+        // }
+        const dk = i % 2 == 1;
+        if(dk) console.log(i);
     }
 }
 function insochia3du1(){
     for(let i = 0 ; i<=100 ; i++ ){
-        if(i % 3 == 1){
-            console.log(i)
-        }
+        // if(i % 3 == 1){
+        //     console.log(i)
+        // }
+        const dk = i % 3 == 1;
+        if(dk) console.log(i);
     }
 }
 function insochinhphuong(){
     for(let i = 0 ; i<=100 ; i++ ){
-        if(Math.sqrt(i) % 1 == 0){
-            console.log(i)
-        }
+        // if(Math.sqrt(i) % 1 == 0){
+        //     console.log(i)
+        // }
+        const dk = Math.sqrt(i) % 1 === 0;
+        if(dk) console.log(i);
     }
 }
 // insochan();
 // insole();
 // insochia3du1();
-insochinhphuong()
+// insochinhphuong()
+
+// function inso(sobichia ,sodu){
+//     for(let i = 0 ; i<=100 ; i++ ){
+//         const dk = i % sobichia == sodu;
+//     }
+// }
+// inso(3,1);
+// insochan();
+// insole()
+// insochia3du1();
+insochinhphuong();
